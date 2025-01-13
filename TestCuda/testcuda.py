2108376cd6ded9063659f15ee665c550b7d1cd74
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-01-13 06:40:44 -0800

    Update

---
+++ TestCuda/testcuda.py
@@ -0,0 +1,10 @@
+import torch
+
+def check_cuda():
+    if torch.cuda.is_available():
+        print("CUDA is available")
+    else:
+        print("CUDA is not available")
+
+if __name__ == "__main__":
+    check_cuda()
