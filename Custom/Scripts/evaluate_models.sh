63ef80201da134a3d163b4a3ab98ed776de8be6c
DPTPhatUS <23125065@student.hcmus.edu.vn>
2025-03-08 22:06:28 +0700

    vit2 test

--- Custom/Scripts/evaluate_models.sh
+++ Custom/Scripts/evaluate_models.sh
@@ -22,7 +22,7 @@ conf_thresh=0.1
 batch_size=16
 dataset="dota"
 phase="eval"
-model_dir="bbav"
+model_dir="vit2"
 # Directory structure:
 # -|weights_dota
 # ---| your_model_weights
@@ -31,11 +31,11 @@ model_dir="bbav"
 # -----| ...
 eval_script="dota_evaluation_task1.py"
 eval_dir="datasets/DOTA_devkit"
-result_dir="Result/bbav"
+result_dir="Result/vit2"
 # A folder named "Result" will be created in the current directory to store the evaluation results
 
 # Array of model epochs to evaluate
-epochs=(50 43)
+epochs=(9 8 7 6 5 4 3 2 1)
 
 # Create the result directory if it doesn't exist
 mkdir -p "Result"
