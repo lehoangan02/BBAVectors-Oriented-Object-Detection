948093bd3c861118a2340417727493cc9e1810fb
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-03-20 21:33:24 +0700

    resume

--- loss.py
+++ loss.py
@@ -152,3 +152,73 @@ class LossAll(torch.nn.Module):
 
         loss =  hm_loss + wh_loss + off_loss + cls_theta_loss+corners_loss
         return loss
+class MSE(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def _gather_feat(self, feat, ind, mask=None):
+        dim = feat.size(2)
+        ind = ind.unsqueeze(2).expand(ind.size(0), ind.size(1), dim)
+        feat = feat.gather(1, ind)
+        if mask is not None:
+            mask = mask.unsqueeze(2).expand_as(feat)
+            feat = feat[mask]
+            feat = feat.view(-1, dim)
+        return feat
+
+    def _tranpose_and_gather_feat(self, feat, ind):
+        feat = feat.permute(0, 2, 3, 1).contiguous()
+        feat = feat.view(feat.size(0), -1, feat.size(3))
+        feat = self._gather_feat(feat, ind)
+        return feat
+
+    def forward(self, output, mask, ind, target):
+        # torch.Size([1, 2, 152, 152])
+        # torch.Size([1, 500])
+        # torch.Size([1, 500])
+        # torch.Size([1, 500, 2])
+        pred = self._tranpose_and_gather_feat(output, ind)  # torch.Size([1, 500, 2])
+        if mask.sum():
+            mask = mask.unsqueeze(2).expand_as(pred).bool()
+            mse_loss = nn.MSELoss(reduction='mean')
+            loss = mse_loss(pred.masked_select(mask),
+                                    target.masked_select(mask))
+            return loss
+        else:
+            return 0.
+class LossAll_mse(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.L_hm = FocalLoss()
+        self.L_wh =  MSE()
+        self.L_off = OffSmoothL1Loss()
+        self.L_cls_theta = BCELoss()
+        self.L_corners = OffSmoothL1Loss()
+
+    def forward(self, pr_decs, gt_batch):
+        hm_loss  = self.L_hm(pr_decs['hm'], gt_batch['hm'])
+        wh_loss  = self.L_wh(pr_decs['wh'], gt_batch['reg_mask'], gt_batch['ind'], gt_batch['wh'])
+        off_loss = self.L_off(pr_decs['reg'], gt_batch['reg_mask'], gt_batch['ind'], gt_batch['reg'])
+        if 'corners' in pr_decs:
+            corners_loss = self.L_corners(pr_decs['corners'], gt_batch['reg_mask'], gt_batch['ind'], gt_batch['corners'])
+        else:
+            corners_loss = 0
+        ## add
+        cls_theta_loss = self.L_cls_theta(pr_decs['cls_theta'], gt_batch['reg_mask'], gt_batch['ind'], gt_batch['cls_theta'])
+
+        if isnan(hm_loss) or isnan(wh_loss) or isnan(off_loss):
+            print('hm loss is {}'.format(hm_loss))
+            print('wh loss is {}'.format(wh_loss))
+            print('off loss is {}'.format(off_loss))
+            print('corners loss is {}'.format(corners_loss))
+
+        print(f"hm_loss: {hm_loss}")
+        print(f"wh_loss: {wh_loss}")
+        print(f"off_loss: {off_loss}")
+        print(f"cls_theta_loss: {cls_theta_loss}")
+        if 'corners' in pr_decs:
+            print(f"corners_loss: {corners_loss}")
+        print('-----------------')
+
+        loss =  hm_loss + wh_loss + off_loss + cls_theta_loss+corners_loss
+        return loss
