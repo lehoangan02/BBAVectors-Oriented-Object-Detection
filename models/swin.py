c269efc3411471a8b20945fd52ffdc8bd571344c
DPTPhatUS <23125065@student.hcmus.edu.vn>
2025-03-28 07:43:54 +0700

    start with swin_t

--- models/swin.py
+++ models/swin.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from torchvision.models import swin_b, Swin_B_Weights
+from torchvision.models import swin_t, Swin_T_Weights, swin_s, Swin_S_Weights,  swin_b, Swin_B_Weights
 
 # class InputProcessor(nn.Module):
 
@@ -11,17 +11,19 @@ class SwinEncoder(nn.Module):
 
         # Load pre-trained Swin model
         if pretrained:
-            weights = Swin_B_Weights.IMAGENET1K_V1
+            weights = Swin_T_Weights.IMAGENET1K_V1
         else:
             weights = None
-        swin = swin_b(weights = weights, progress = True)
+        swin = swin_t(weights = weights, progress = True)
 
         self.encoder = swin.features
 
         self.extract1 = nn.Sequential(
-            nn.LayerNorm(128, eps=1e-5),
+            nn.LayerNorm(96, eps=1e-5), # Swin_T
+            # nn.LayerNorm(128, eps=1e-5), # Swin_B
             swin.permute,
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=1), # Swin_T
+            # nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), # Swin_B
             # nn.BatchNorm2d(256),
             nn.LayerNorm((256, 152, 152), eps=1e-5),
             nn.ReLU(inplace=True),
@@ -29,9 +31,11 @@ class SwinEncoder(nn.Module):
             # nn.BatchNorm2d(256)
         )
         self.extract2 = nn.Sequential(
-            nn.LayerNorm(256, eps=1e-5),
+            nn.LayerNorm(192, eps=1e-5), # Swin_T
+            # nn.LayerNorm(256, eps=1e-5), # Swin_B
             swin.permute,
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(192, 512, kernel_size=3, stride=1, padding=1), # Swin_T
+            # nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), # Swin_B
             # nn.BatchNorm2d(512),
             nn.LayerNorm((512, 76, 76), eps=1e-5),
             nn.ReLU(inplace=True),
@@ -39,9 +43,11 @@ class SwinEncoder(nn.Module):
             # nn.BatchNorm2d(512)
         )
         self.extract3 = nn.Sequential(
-            nn.LayerNorm(512, eps=1e-5),
+            nn.LayerNorm(384, eps=1e-5), # Swin_T
+            # nn.LayerNorm(512, eps=1e-5), # Swin_B
             swin.permute,
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(384, 1024, kernel_size=3, stride=1, padding=1), # Swin_T
+            # nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), # Swin_B
             # nn.BatchNorm2d(1024),
             nn.LayerNorm((1024, 38, 38), eps=1e-5),
             nn.ReLU(inplace=True),
@@ -51,7 +57,8 @@ class SwinEncoder(nn.Module):
         self.extract4 = nn.Sequential(
             swin.norm,
             swin.permute,
-            nn.Conv2d(1024, 2048, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(768, 2048, kernel_size=3, stride=1, padding=1), # Swin_T
+            # nn.Conv2d(1024, 2048, kernel_size=3, stride=1, padding=1), # Swin_B
             # nn.BatchNorm2d(2048),
             nn.LayerNorm((2048, 19, 19), eps=1e-5),
             nn.ReLU(inplace=True),
